c6cdd38379be91546e24c8fad5ed594fcf756a2d
Acid Chicken (硫酸鶏) <root@acid-chicken.com>
2019-03-15 22:12:48 +0900

    Enhance share page

--- src/client/app/common/views/pages/share.vue
+++ src/client/app/common/views/pages/share.vue
@@ -1,10 +1,10 @@
 <template>
 <div class="azibmfpleajagva420swmu4c3r7ni7iw">
 	<h1>{{ $t('share-with', { name }) }}</h1>
-	<div>
+	<div :class="{ fluid }">
 		<mk-signin v-if="!$store.getters.isSignedIn"/>
 		<mk-post-form v-else-if="!posted" :initial-text="template" :instant="true" @posted="posted = true"/>
-		<p v-if="posted" class="posted"><fa icon="check"/></p>
+		<p v-else class="posted"><fa icon="check"/></p>
 	</div>
 	<ui-button class="close" v-if="posted" @click="close">{{ $t('@.close') }}</ui-button>
 </div>
@@ -17,12 +17,14 @@ import i18n from '../../../i18n';
 export default Vue.extend({
 	i18n: i18n('mobile/views/pages/share.vue'),
 	data() {
+		const params = new URLSearchParams(location.search);
 		return {
 			name: null,
 			posted: false,
-			text: new URLSearchParams(location.search).get('text'),
-			url: new URLSearchParams(location.search).get('url'),
-			title: new URLSearchParams(location.search).get('title'),
+			text: params.get('text'),
+			url: params.get('url'),
+			title: params.get('title'),
+			fluid: params.get('fluid')
 		};
 	},
 	computed: {
@@ -57,15 +59,20 @@ export default Vue.extend({
 		text-align center
 
 	> div
-		max-width 500px
 		margin 0 auto
+		max-width 544px
+
+		&.fluid
+			margin 0 8px
+			max-width none
 
 		> .posted
 			display block
 			margin 0 auto
 			padding 64px
 			text-align center
-			background #fff
+			background var(--bg)
+			color var(--text)
 			border-radius 6px
 			width calc(100% - 32px)
 
