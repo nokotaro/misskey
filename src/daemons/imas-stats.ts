ffb984a57802993fa94790520ccccb50acc17ccd
Acid Chicken (硫酸鶏) <root@acid-chicken.com>
2019-03-15 21:15:00 +0900

    Add polling access

--- src/daemons/imas-stats.ts
+++ src/daemons/imas-stats.ts
@@ -1,4 +1,5 @@
 import * as WebSocket from 'ws';
+import * as request from 'request-promise-native';
 import config from '../config';
 import { query } from '../prelude/url';
 import { createNote } from '../remote/activitypub/models/note';
@@ -6,8 +7,11 @@ import { createNote } from '../remote/activitypub/models/note';
 const interval = 1024;
 
 export default function() {
-	const connect = (url: string) => {
-		const socket = new WebSocket(url);
+	const connect = (host: string, token: string) => {
+		const socket = new WebSocket(`https://${host}/api/v1/streaming?${query({
+			access_token: token,
+			stream: 'public:local'
+		})}`);
 
 		socket.on('message', x => {
 			try {
@@ -28,12 +32,24 @@ export default function() {
 			}
 		});
 
+		socket.on('open', () => request({
+				url: `https://${host}/api/v1/timelines/public?${query({
+					local: true,
+					limit: 40
+				})}`,
+				proxy: config.proxy,
+				headers: {
+					'User-Agent': config.userAgent,
+					Accept: 'application/json+oembed, application/json'
+				},
+				json: true
+			}).then(response => Array.isArray(response) && response
+				.filter(x => typeof x === 'object' && typeof x.uri === 'string')
+				.reduceRight<Promise<void>>((a, c) => a.then(() => createNote(c)).then(() => {}), Promise.resolve())));
+
 		socket.on('close', _ => setTimeout(connect, interval, socket));
 	};
 
 	for (const [k, v] of Object.entries(config.imasHostTokens))
-		connect(`https://${k}/api/v1/streaming?${query({
-			access_token: v,
-			stream: 'public:local'
-		})}`);
+		connect(k, v);
 }
