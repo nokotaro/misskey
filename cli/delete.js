75d8f8a3859fec64fdd60b00a6f6ac1f26692945
Acid Chicken (硫酸鶏) <root@acid-chicken.com>
2019-03-19 01:29:27 +0900

    Update cli script

--- cli/delete.js
+++ cli/delete.js
@@ -1,24 +1,6 @@
 const { default: DriveFile } = require('../built/models/drive-file');
 const { default: config } = require('../built/config');
 
-const getExt = file => {
-	const [x] = (file.filename.match(/\.([a-zA-Z0-9_-]+)$/) || []);
-
-	if (x)
-		return x;
-
-	if (file.contentType === 'image/jpeg')
-		return '.jpg';
-
-	if (file.contentType === 'image/png')
-		return '.png';
-
-	if (file.contentType === 'image/webp')
-		return '.webp';
-
-	return '';
-};
-
 async function main() {
 	const files = await DriveFile.find();
 	const localFiles = files.filter(x => !x.metadata.deletedAt && !x.metadata.isRemote);
@@ -29,7 +11,12 @@ async function main() {
 
 	for (const file of localFiles) {
 		// const ext = getExt(file, file.contentType);
-		console.log(file);
+		await DriveFile.findOneAndUpdate({ _id: file._id }, {
+			$set: {
+				deletedAt: new Date(),
+				isExpired: false
+			}
+		})
 	}
 
 	process.exit(0);
