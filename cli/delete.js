d9ba1069a09f5e4284996611be05f0b2c121481d
Acid Chicken (硫酸鶏) <root@acid-chicken.com>
2019-03-19 15:39:00 +0900

    Update delete cli

--- cli/delete.js
+++ cli/delete.js
@@ -1,22 +1,17 @@
 const { default: DriveFile } = require('../built/models/drive-file');
 const { default: config } = require('../built/config');
+const { default: deleteFile } = require('../built/services/drive/delete-file');
 
 async function main() {
 	const files = await DriveFile.find();
-	const localFiles = files.filter(x => !x.metadata.deletedAt && !x.metadata.isRemote);
+	const localFiles = files.filter(x => !x.metadata.isRemote);
 	if (config.drive.storage !== 'minio') {
 		console.error('ERROR: Set drive storage to minio.');
 		process.exit(1);
 	}
 
 	for (const file of localFiles) {
-		// const ext = getExt(file, file.contentType);
-		await DriveFile.findOneAndUpdate({ _id: file._id }, {
-			$set: {
-				deletedAt: new Date(),
-				isExpired: false
-			}
-		})
+		deleteFile(file);
 	}
 
 	process.exit(0);
