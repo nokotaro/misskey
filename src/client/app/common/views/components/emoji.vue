db1d7d8fc0be06fb5ef1aa8b6b54a939f9fd45ad
Acid Chicken (硫酸鶏) <root@acid-chicken.com>
2019-03-24 14:45:46 +0900

    Update emoji url

--- src/client/app/common/views/components/emoji.vue
+++ src/client/app/common/views/components/emoji.vue
@@ -1,6 +1,6 @@
 <template>
 <img v-if="customEmoji" class="fvgwvorwhxigeolkkrcderjzcawqrscl custom" :class="{ normal }" :src="url" :alt="alt" :title="alt"/>
-<img v-else-if="!char" class="fvgwvorwhxigeolkkrcderjzcawqrscl custom unknown" :class="{ normal }" :src="`${config.url}/assets/emojis/${name}`" :alt="`:${name}:`" :title="`:${name}:`"/>
+<img v-else-if="!char" class="fvgwvorwhxigeolkkrcderjzcawqrscl custom unknown" :class="{ normal }" :src="$store.state.device.disableShowingAnimatedImages ? `${config.url}/assets/emojis/${name}` : `${config.url}/proxy/${name}.png?url=${encodeURIComponent(`${config.url}/assets/emojis/${name}`)}&static=1`" :alt="`:${name}:`" :title="`:${name}:`"/>
 <span v-else-if="useOsDefaultEmojis">{{ char }}</span>
 <img v-else class="fvgwvorwhxigeolkkrcderjzcawqrscl" :src="url" :alt="alt" :title="alt"/>
 </template>
