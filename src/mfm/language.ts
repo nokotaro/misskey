3fa79c0428fb8ae69e5db2dca7df7257dae1a11a
Acid Chicken (硫酸鶏) <root@acid-chicken.com>
2019-04-27 18:27:31 +0900

    Fix MFM language

--- src/mfm/language.ts
+++ src/mfm/language.ts
@@ -232,6 +232,8 @@ export const mfmLanguage = P.createLanguage({
 				if (!match) {
 					return P.makeFailure(i, 'not a url');
 				}
+				url = match[1];
+				i += 2;
 			} else {
 				url = match[0];
 			}
