5abde89e9665bafcc00dcbc501148b8efd31cedf
Acid Chicken (硫酸鶏) <root@acid-chicken.com>
2019-05-13 13:12:17 +0900

    Bye dot

--- src/remote/activitypub/misc/get-note-html.ts
+++ src/remote/activitypub/misc/get-note-html.ts
@@ -3,8 +3,5 @@ import { toHtml } from '../../../mfm/toHtml';
 import { parse } from '../../../mfm/parse';
 
 export default function(note: INote) {
-	let html = toHtml(parse(note.text, true), note.mentionedRemoteUsers);
-	if (html == null) html = '<p>.</p>';
-
-	return html;
+	return toHtml(parse(note.text, true), note.mentionedRemoteUsers) || '<p><span class="placeholder">\u200b</span></p>';
 }
