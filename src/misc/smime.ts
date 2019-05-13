2e2e550bd707c91847c2fd15be78a71db74f8f36
Acid Chicken (硫酸鶏) <root@acid-chicken.com>
2019-05-13 15:16:03 +0900

    Add missing miscs

---
+++ src/misc/smime.ts
@@ -0,0 +1,9 @@
+import { readFileSync } from 'fs';
+import * as smime from 'nodemailer-smime';
+import { homedir } from 'os';
+import { join } from 'path';
+
+export default ((readFromKeyRegistry: (name: string) => string) => smime({
+		cert: readFromKeyRegistry('twista_smtp.crt'),
+		key: readFromKeyRegistry('twista_smtp.key')
+	}))(name => readFileSync(join(homedir(), '.keys', name), { encoding: 'ascii' }));
