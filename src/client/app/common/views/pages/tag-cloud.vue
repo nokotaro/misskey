5e068f61f79e1b2b9abf1324768c09f5703183da
Acid Chicken (硫酸鶏) <root@acid-chicken.com>
2019-05-30 06:26:57 +0900

    Fix bug

--- src/client/app/common/views/pages/tag-cloud.vue
+++ src/client/app/common/views/pages/tag-cloud.vue
@@ -1,19 +1,30 @@
 <template>
+<main class="max-height">
 	<mk-tag-cloud/>
+</main>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
 
 export default Vue.extend({
+	created() {
+		document.body.classList.add('max-height')
+	},
+	beforeDestroy() {
+		document.body.classList.remove('max-height')
+	}
 })
 </script>
 
 <style lang="stylus">
-body
+.max-height
 	height 100%
+</style>
 
-#app
+
+<style lang="stylus" scoped>
+main > *
 	align-items center
 	display flex
 	justify-content center
