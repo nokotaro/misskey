91eaf81c4009df741c88e3c4fa8933ed3f0593a3
Acid Chicken (硫酸鶏) <root@acid-chicken.com>
2019-06-01 03:15:42 +0900

    Fix bug

--- src/server/ostatus.ts
+++ src/server/ostatus.ts
@@ -19,7 +19,7 @@ router.get(authorizeInteractionPath, async ctx => {
 		await (async x =>
 			(x => x && `${config.url}/users/${x._id}`)(await createPersonFromObject(x.url, x, resolver).catch(() => null)) ||
 			(x => x && `${config.url}/notes/${x._id}`)(await createNote(x, resolver, true).catch(() => null))
-		)(await resolver.resolve(acct));
+		)(await resolver.resolve(acct).catch(() => null));
 
 	if (url)
 		ctx.redirect(url);
