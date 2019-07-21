9f5078ac1cba36755c6da3e4a604691a890196da
Acid Chicken (硫酸鶏) <root@acid-chicken.com>
2019-07-21 18:32:48 +0900

    Improve imas tl worker

--- src/daemons/imas-tl-worker.ts
+++ src/daemons/imas-tl-worker.ts
@@ -6,50 +6,56 @@ import { createNote } from '../remote/activitypub/models/note';
 
 const interval = 1024;
 
-export default function() {
-	const connect = (host: string, token: string) => {
-		const socket = new WebSocket(`https://${host}/api/v1/streaming?${query({
-			access_token: token,
-			stream: 'public:local'
-		})}`);
+function work() {
+	try {
+		const connect = (host: string, token: string) => {
+			const socket = new WebSocket(`https://${host}/api/v1/streaming?${query({
+				access_token: token,
+				stream: 'public:local'
+			})}`);
 
-		socket.on('message', x => {
-			try {
-				const data: {
-					event?: string,
-					payload?: string
-				} = JSON.parse(typeof x === 'string' ? x : x.toString());
+			socket.on('message', x => {
+				try {
+					const data: {
+						event?: string,
+						payload?: string
+					} = JSON.parse(typeof x === 'string' ? x : x.toString());
 
-				if (data.event === 'update' && typeof data.payload === 'string') {
-					const payload: {
-						uri?: string
-					} = JSON.parse(data.payload);
+					if (data.event === 'update' && typeof data.payload === 'string') {
+						const payload: {
+							uri?: string
+						} = JSON.parse(data.payload);
 
-					if (typeof payload.uri === 'string')
-						createNote(payload.uri);
+						if (typeof payload.uri === 'string')
+							createNote(payload.uri);
+					}
+				} finally {
 				}
-			} finally {
-			}
-		});
+			});
 
-		socket.on('open', () => request({
-				url: `https://${host}/api/v1/timelines/public?${query({
-					local: true,
-					limit: 40
-				})}`,
-				proxy: config.proxy,
-				headers: {
-					'User-Agent': config.userAgent,
-					Accept: 'application/json+oembed, application/json'
-				},
-				json: true
-			}).then(response => Array.isArray(response) && response
-				.filter(x => typeof x === 'object' && typeof x.uri === 'string')
-				.reduceRight<Promise<void>>((a, c) => a.then(() => createNote(c)).then(() => {}), Promise.resolve())));
+			socket.on('open', () => request({
+					url: `https://${host}/api/v1/timelines/public?${query({
+						local: true,
+						limit: 40
+					})}`,
+					proxy: config.proxy,
+					headers: {
+						'User-Agent': config.userAgent,
+						Accept: 'application/json+oembed, application/json'
+					},
+					json: true
+				}).then(response => Array.isArray(response) && response
+					.filter(x => typeof x === 'object' && typeof x.uri === 'string')
+					.reduceRight<Promise<void>>((a, c) => a.then(() => createNote(c)).then(() => {}), Promise.resolve())));
 
-		socket.on('close', _ => setTimeout(connect, interval, socket));
-	};
+			socket.on('close', _ => setTimeout(connect, interval, socket));
+		};
 
-	for (const [k, v] of Object.entries(config.imasHostTokens))
-		connect(k, v);
+		for (const [k, v] of Object.entries(config.imasHostTokens))
+			connect(k, v);
+	} catch (e) {
+		setTimeout(work, interval);
+	}
 }
+
+export default work;
