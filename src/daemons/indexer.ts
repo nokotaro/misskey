17752883ad30365386b9a35f1a2f19c583585495
Acid Chicken (硫酸鶏) <root@acid-chicken.com>
2019-07-23 13:02:43 +0900

    Create indexer daemon

---
+++ src/daemons/indexer.ts
@@ -0,0 +1,30 @@
+import { getIndexer } from '../misc/mecab';
+import Note from '../models/note';
+
+const interval = 1000;
+
+export async function index() {
+	const note = await Note.findOne({
+		mecabIndex: { $exists: false }
+	}).catch(_ => null);
+
+	if (!note) {
+		return false;
+	}
+
+	const { _id } = note;
+
+	await Note.findOneAndUpdate({ _id }, {
+		$set: {
+			mecabIndex: await getIndexer(note)
+		}
+	}).catch(_ => {});
+
+	return true;
+}
+
+function work() {
+	index().then(x => x && setTimeout(work, interval), _ => setTimeout(work, interval));
+}
+
+export default work;
