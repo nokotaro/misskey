b01650101525012d023c7938458635c35ed747bd
Acid Chicken (硫酸鶏) <root@acid-chicken.com>
2019-08-05 03:56:42 +0900

    Fix typings

--- src/server/web/feed/util.ts
+++ src/server/web/feed/util.ts
@@ -1,10 +1,10 @@
-import xmlbuilder = require('xmlbuilder');
+import { create } from 'xmlbuilder';
 
 /**
  * Convert to XML
  * @param obj source object
  */
 export function objectToXml(obj: {}): string {
-	const xml = xmlbuilder.create(obj, { encoding: 'utf-8' });
+	const xml = create(obj, { encoding: 'utf-8' });
 	return xml.end({ pretty: true });
 }
