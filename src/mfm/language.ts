6c68dca254627642f977565e5c788390a95f7be0
Acid Chicken (硫酸鶏) <root@acid-chicken.com>
2019-09-09 03:10:54 +0900

    Fix bug

--- src/mfm/language.ts
+++ src/mfm/language.ts
@@ -403,9 +403,10 @@ export const mfmLanguage = P.createLanguage({
 		const name = P((input, i) => {
 			const text = input.substr(i);
 			const [full, name] = text.match(/^:(@?[\w-]+(?:@[\w.-]+)?):(?:\u200b*$)?/i) || [null, null];
-			return name && !name.match(/^[\d-]*$/) ? P.makeSuccess(i + full.length, { name }) : P.makeFailure(i, 'not an emoji');
+			const raw = `:${name}:`;
+			return name && !name.match(/^[\d-]*$/) ? P.makeSuccess(i + full.length, { name, raw }) : P.makeFailure(i, 'not an emoji');
 		});
-		const code = P.regexp(emojiRegex).map(emoji => ({ emoji }));
+		const code = P.regexp(emojiRegex).map(emoji => ({ emoji, raw: emoji }));
 		return P.alt(name, code).map(x => createLeaf('emoji', x));
 	},
 	text: () => P.any.map(x => createLeaf('text', { text: x }))
