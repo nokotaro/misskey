d061da6b6b8152f10e97b8395ca6fd17fcc4c857
Acid Chicken (硫酸鶏) <root@acid-chicken.com>
2019-09-17 20:06:02 +0900

    Tune twista boot loader

--- src/client/app/safe.js
+++ src/client/app/safe.js
@@ -1,13 +1,1 @@
-/**
- * ブラウザの検証
- */
-
-// Detect an old browser
-if (!('fetch' in window)) {
-	alert(
-		'お使いのブラウザ(またはOS)のバージョンが旧式のため、twistaを動作させることができません。' +
-		'バージョンを最新のものに更新するか、別のブラウザをお試しください。' +
-		'\n\n' +
-		'Your browser (or your OS) seems outdated. ' +
-		'To run twista, please update your browser to latest version or try other browsers.');
-}
+window.fetch||alert('unsupported browser')
