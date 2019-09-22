3c4278faf043ececdea3373a96644123f791f4d2
Acid Chicken (硫酸鶏) <root@acid-chicken.com>
2019-09-23 01:40:14 +0900

    Revert "Roll back"
    This reverts commit 04dc4f3646c75395c824782f55801bc6bf4cecd7.

--- src/client/app/boot.js
+++ src/client/app/boot.js
@@ -1,157 +1,21 @@
-'use strict';
-
-(async function() {
-	// キャッシュ削除要求があれば従う
-	if (localStorage.getItem('shouldFlush') == 'true') {
-		refresh();
-		return;
-	}
-
-	const langs = LANGS;
-
-	//#region Apply theme
-	const theme = localStorage.getItem('theme');
-	if (theme) {
-		for (const [k, v] of Object.entries(JSON.parse(theme))) {
-			document.documentElement.style.setProperty(`--${k}`, v.toString());
-		}
-	}
-	//#endregion
-
-	//#region Load settings
-	let settings = null;
-	const vuex = localStorage.getItem('vuex');
-	if (vuex) {
-		settings = JSON.parse(vuex);
-	}
-	//#endregion
-
-	// Get the current url information
-	const url = new URL(location.href);
-
-	//#region Detect app name
-	let app = null;
-
-	// if (`${url.pathname}/`.startsWith('/docs/')) app = 'docs';
-	if (`${url.pathname}/`.startsWith('/dev/')) app = 'dev';
-	if (`${url.pathname}/`.startsWith('/auth/')) app = 'auth';
-	if (`${url.pathname}/`.startsWith('/admin/')) app = 'admin';
-	if (`${url.pathname}/`.startsWith('/test/')) app = 'test';
-	if (`${url.pathname}/`.startsWith('/calc/')) app = 'calc';
-	if (`${url.pathname}/`.startsWith('/terms/')) app = 'terms';
-	//#endregion
-
-	// Script version
-	const ver = localStorage.getItem('v') || VERSION;
-
-	//#region Detect the user language
-	let lang = null;
-
-	if (langs.includes(navigator.language)) {
-		lang = navigator.language;
-	} else {
-		lang = langs.find(x => x.split('-')[0] == navigator.language);
-
-		if (lang == null) {
-			// Fallback
-			lang = 'en-US';
-		}
-	}
-
-	if (settings && settings.device.lang && langs.includes(settings.device.lang)) {
-		lang = settings.device.lang;
-	}
-
-	localStorage.setItem('lang', lang);
-	//#endregion
-
-	//#region Fetch locale data
-	const cachedLocale = localStorage.getItem('locale');
-	const localeKey = localStorage.getItem('localeKey');
-
-	if (cachedLocale == null || localeKey != `${ver}.${lang}`) {
-		const locale = await fetch(`/assets/locales/${lang}.json?ver=${ver}`)
-			.then(response => response.json());
-
-		localStorage.setItem('locale', JSON.stringify(locale));
-		localStorage.setItem('localeKey', `${ver}.${lang}`);
-	}
-	//#endregion
-
-	// Detect the user agent
-	const ua = navigator.userAgent.toLowerCase();
-	const isMobile = /mobile|iphone|ipad|android/.test(ua);
-
-	// Get the <head> element
-	const head = document.getElementsByTagName('head')[0];
-
-	// If mobile, insert the viewport meta tag
-	if (isMobile) {
-		const meta = document.createElement('meta');
-		meta.setAttribute('name', 'viewport');
-		meta.setAttribute('content',
-			'width=device-width,' +
-			'initial-scale=1,' +
-			'minimum-scale=1,' +
-			'maximum-scale=1,' +
-			'user-scalable=no');
-		head.appendChild(meta);
-	}
-
-	// Switch desktop or mobile version
-	if (app == null) {
-		app = isMobile ? 'mobile' : 'desktop';
-	}
-
-	// Load an app script
-	// Note: 'async' make it possible to load the script asyncly.
-	//       'defer' make it possible to run the script when the dom loaded.
-	const script = document.createElement('script');
-	script.setAttribute('src', `/assets/${app}.${ver}.js`);
-	script.setAttribute('async', 'true');
-	script.setAttribute('defer', 'true');
-	head.appendChild(script);
-
-	// 3秒経ってもスクリプトがロードされない場合はバージョンが古くて
-	// 404になっているせいかもしれないので、バージョンを確認して古ければ更新する
-	//
-	// 読み込まれたスクリプトからこのタイマーを解除できるように、
-	// グローバルにタイマーIDを代入しておく
-	window.mkBootTimer = window.setTimeout(async () => {
-		// Fetch meta
-		const res = await fetch('/api/meta', {
-			method: 'POST',
-			cache: 'no-cache'
-		});
-
-		// Parse
-		const meta = await res.json();
-
-		// Compare versions
-		if (meta.version != ver) {
-			localStorage.setItem('v', meta.version);
-
-			refresh();
-		}
-	}, 3000);
-
-	function refresh() {
-		localStorage.setItem('shouldFlush', 'false');
-
-		localStorage.removeItem('locale');
-
-		// Clear cache (service worker)
-		try {
-			navigator.serviceWorker.controller.postMessage('clear');
-
-			navigator.serviceWorker.getRegistrations().then(registrations => {
-				for (const registration of registrations) registration.unregister();
-			});
-		} catch (e) {
-			console.error(e);
-		}
-
-		// Force reload
-		location.reload(true);
-	}
-})();
+'use strict'
+!function(g,i,r,o,b,k){!function(d,n,s,t,h,l,j,a,f,k,m,p,q,w,x,y,z,$,_,B,C,D){function A(){s[z](a)
+s[z](o)
+try{!function(n){!function(c){c&&c.postMessage('clear')
+n.getRegistrations()[m](function(x){x[p](function(_,c){c.unregister()},0)})}(n.controller)}(n.serviceWorker)}catch(e){console.error(e)}t.reload(!0)}s[x](D)&&(d[B][C].background='#000',d.addEventListener('DOMContentLoaded',function(){s[x](D)&&(d.body[C].opacity=0)}))
+s[x](a)&&A()
+!function(t){t&&Object.entries(j.parse(t))[p](function(_,x){d[B][C][b+'Property']('--'+x[0],x[1])},0)}(s[x]('theme'))
+!function(c,a,v,u,i,e,g,z){c&&!function(d){l[q](d)&&(u=d)}(c.device[r])
+s[y](r,u)
+!function(c,k,d){c&&k==d||h(_+o+'s/'+u+z+g+'?'+v)[m](function(x){x[g]()[m](function(x){s[y](o,j.stringify(x))
+s[y](f,d)})})}(s[x](o),s[x](f),v+z+u)
+a=a||(i?'mobile':'desktop')
+!function(k,b){k[b]('src',_+a+z+v+'.js')
+k[b]('async',!0)
+k[b]('defer',!0)
+e.appendChild(k)}(d.createElement('script'),b+'Attribute')
+!function(q,p){q[p]='no-'+p
+window.mkBootTimer=setTimeout(function(z){h('/api/meta',q)[m](function(x){x[g]()[m](function(x){x[z]!=v&&s[y](k,x[z])
+A()})})},3e3,'version')}({method:'POST'},'cache')
+}((function(v){return v&&j.parse(v)})(s[x]('vuex')),(function(x){return ['admin','auth','calc','dev','terms','test'][p](function(a,c){return a||x.startsWith($+c+$)&&c},0)})(new
+URL(t.href).pathname+$),s[x](k)||VERSION,l[q](n[w])?n[w]:l[p](function(a,c){return a||(function(r){return n[w]==r&&r})(c.split('-')[0])},0)||'en-US',/[Mm]obile|iP|Android/.test(n.userAgent),d.head,'json','.')}(document,navigator,localStorage,location,fetch,LANGS,JSON,'shouldFlush',o+k,'v','then','reduce','includes',r+'uage',g+i,b+i,'remove'+i,'/',/assets/,'documentElement','style','update_with_dimmer')}('get','Item','lang','locale','set','Key')
